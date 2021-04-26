0d202fee7e386427702ad2198fa229b8e57ae164
Daniel Montes Guerrero <danimontesguerrero17@gmail.com>
2021-04-25 21:43:51 -0500

    Revert "Update gitingnore"
    This reverts commit 87f642d837b8017f1244e8af3fcf18e05403f171.

--- LanguageTesting/Assets/Scripts/GeneratorEditor.cs
+++ LanguageTesting/Assets/Scripts/GeneratorEditor.cs
@@ -2,23 +2,15 @@ using UnityEngine;
 using UnityEditor;
 
 [CustomEditor(typeof(Generator))]
-public class GeneratorEditor : Editor
-{
-	public override void OnInspectorGUI()
-	{
+public class GeneratorEditor : Editor{
+	public override void OnInspectorGUI(){
 		base.OnInspectorGUI();
 		Generator generator = (Generator) target;
-		if (GUILayout.Button("Generate Map"))
-		{
+		if (GUILayout.Button("Generate Map")){
 			generator.GenerateMap();
 		}
-		if (GUILayout.Button("Clear Map"))
-		{
+		if (GUILayout.Button("Clear Map")){
 			generator.ClearMap();
-		}
-		if (GUILayout.Button("Generate All Rule Tiles"))
-		{
-			generator.GenerateAllRuleTiles();
 		}
 	}
 }
